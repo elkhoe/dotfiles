3f7007614eb2052a887f04600814c93f5da2a7da
Ethan Khoe <ekhoe@premierlacrosseleague.com>
2025-07-17 15:52:10 -0400

    update link.sh

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Creates symbolic links from your dotfiles repo to your home directory
-ln -s ~/repos/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
